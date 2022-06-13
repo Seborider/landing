f7117d96d6f4082771b4c62018f2e622841639fc
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-13 16:41:39 +0200

    🎨 Subscribe to getCurrentLocation

--- src/app/weather/forecast/forecast.component.ts
+++ src/app/weather/forecast/forecast.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ForecastService } from '../forecast.service';
 
 @Component({
   selector: 'app-forecast',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForecastComponent implements OnInit {
 
-  constructor() { }
+  constructor(forecastService: ForecastService) { 
+    forecastService.getCurrentLocation().subscribe((coords) => {
+      console.log(coords);
+      
+    })
+  }
 
   ngOnInit(): void {
   }

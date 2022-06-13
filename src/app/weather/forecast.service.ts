0b871fe0ce183dc29d33ead93c10788217989003
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-13 17:01:34 +0200

    🎨 Add getForecast with HttpParams

--- src/app/weather/forecast.service.ts
+++ src/app/weather/forecast.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
+import { HttpParams } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +8,18 @@ import { Observable } from 'rxjs';
 export class ForecastService {
 
   constructor() { }
+
+  getForecast(){
+   return this.getCurrentLocation().pipe(
+      map(coords => {
+        return new HttpParams()
+        .set('lat', String(coords.latitude))
+        .set('lon', String(coords.longitude))
+        .set('units', 'metric')
+        .set('appid', '2b52e06b5ddf84045cc22f2caffe7921')
+      })
+    )
+  }
 
   getCurrentLocation() {
     return new Observable<GeolocationCoordinates>((observer) => {

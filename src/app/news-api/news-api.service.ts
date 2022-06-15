e9a967979d15e591e58e3f5a3386a440515bdd1c
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-15 09:18:40 +0200

    🎨 Add Subject declarations

--- src/app/news-api/news-api.service.ts
+++ src/app/news-api/news-api.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Subject, tap, map, switchMap, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +10,13 @@ export class NewsApiService {
   private apiKey = '394893550f72462f826c57c9142aa6d0'
   private country = 'de'
 
-  constructor() { }
+  pagesInput: Subject<number>
+  pagesOutput: Observable<any>
+  numberOfPages: Observable<number>
+
+  constructor() {
+    this.pagesInput = new Subject()
+    this.pagesOutput = this.pagesInput.pipe()
+    this.numberOfPages = new Observable()
+  }
 }

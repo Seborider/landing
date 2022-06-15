cdd4547fa2c81bd94021ffa6f6684f83a32ba281
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-15 09:10:09 +0200

    🎨 Add proberties

--- src/app/news-api/news-api.service.ts
+++ src/app/news-api/news-api.service.ts
@@ -4,6 +4,10 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class NewsApiService {
+  private url = 'https://newsapi.org/v2/top-headlines'
+  private pageSize = 10
+  private apiKey = '394893550f72462f826c57c9142aa6d0'
+  private country = 'de'
 
   constructor() { }
 }
